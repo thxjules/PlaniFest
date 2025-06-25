e142d9b4641d8fd537c20fd3abd6895b638f0089
Julieth Gómez <gomezxiomara08@gmail.com>
2025-06-25 08:22:57 -0500

    Implementación de springSegurity

--- src/main/java/com/example/planifest/repository/UserRepository.java
+++ src/main/java/com/example/planifest/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.example.planifest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 import com.example.planifest.entity.User;
 import com.example.planifest.enums.Role;
 
@@ -11,15 +11,10 @@ import com.example.planifest.enums.Role;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
   
-    
-    // Consulta por nombre de usuario
     User findByUsername(String username);
-    
-    // Consulta por correo electrónico
-    User findByEmail(String email); // Cambia el tipo de retorno a User
+ 
+    Optional<User> findByEmail(String email);
 
-
-    //Consulta por rol
     User findByRole(Role role);
 
 
