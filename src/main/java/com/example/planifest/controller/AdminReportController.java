1ad4c8972e4e29bd6c58a73f7b4c1329311f34e8
Julieth Gómez <gomezxiomara08@gmail.com>
2025-11-15 21:31:01 -0500

    Mejora de vistas

--- src/main/java/com/example/planifest/controller/AdminReportController.java
+++ src/main/java/com/example/planifest/controller/AdminReportController.java
@@ -63,7 +63,7 @@ public ResponseEntity<byte[]> generateAdminReport() throws Exception {
 
     // Logo exclusivo para reporte admin
     String logoAdmin = reportService.encodeImageToBase64(
-        "src/main/resources/static/images/dashboard/logoplanifest.png"
+        "src/main/resources/static/images/planifestblanco.png"
     );
     data.put("logoBase64", logoAdmin);
 
