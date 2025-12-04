74abce3514e247a9039dca691d37bd209fa90ad8
Stephanny9313 <bivistephy@gmail.com>
2025-12-04 14:10:03 -0500

    chabot

--- src/main/java/com/example/planifest/config/SecurityConfig.java
+++ src/main/java/com/example/planifest/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
             .requestMatchers(
                 "/", "/index", "/css/**", "/js/**", "/images/**",
                 "/registro", "/registro/**", "/login", "/error", "/404",
-                "/403", "/500", "/general","/chat", "/bot/**"  
+                "/403", "/500", "/general","/chatbot/**"  
             ).permitAll()
             .anyRequest().authenticated()
         )
--- src/main/java/com/example/planifest/controller/controllerview/ChatBotController.java
+++ src/main/java/com/example/planifest/controller/controllerview/ChatBotController.java
@@ -16,7 +16,7 @@ public class ChatBotController {
 
     @GetMapping
     public String showChat() {
-        return "chat"; // carga chat.html
+        return "chat";
     }
 
     @PostMapping("/message")
@@ -24,12 +24,10 @@ public class ChatBotController {
     public Map<String, String> chatResponse(@RequestBody Map<String, String> body) {
 
         String userText = body.get("message");
-
-        String botReply = "Procesé tu mensaje: " + userText;
+        String reply = "Procesé tu mensaje: " + userText;
 
         Map<String, String> res = new HashMap<>();
-        res.put("reply", botReply);
-
+        res.put("reply", reply);
         return res;
     }
 }
