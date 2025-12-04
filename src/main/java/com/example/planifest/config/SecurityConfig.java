8325b053bcf1431f15e1b6908c5533e7bdddf96f
Stephanny9313 <bivistephy@gmail.com>
2025-12-04 13:18:31 -0500

    chabot

--- src/main/java/com/example/planifest/config/SecurityConfig.java
+++ src/main/java/com/example/planifest/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
             .requestMatchers(
                 "/", "/index", "/css/**", "/js/**", "/images/**",
                 "/registro", "/registro/**", "/login", "/error", "/404",
-                "/403", "/500", "/general"
+                "/403", "/500", "/general","/chat", "/bot/**"  
             ).permitAll()
             .anyRequest().authenticated()
         )
